3d6d86c34a6a863c00f14fbf6cac8bd1f17905be
Krizzchanne Cid <kcid@cambridge.org>
2024-11-29 15:14:05 +0800

    initial commit

---
+++ cypress/e2e/challenge.cy.ts
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('Sauce Demo Challenge', () => {
+
+  const visitSauceDemo = () => {
+    cy.visit('');
+  };
+
+  const login = (username, password) => {
+    cy.get('#user-name').type(username);
+    cy.get('#password').type(password);
+    cy.get('#login-button').click();
+  };
+
+  const validateLoginSuccess = () => {
+    // Validate successful login by checking if we are redirected to the inventory page
+    cy.url().should('include', '/inventory.htm');
+    cy.get('.inventory_list').should('exist'); // Ensure the inventory list is visible
+  };
+
+  const validateHomePage = () => {
+    // Verify that the URL is correct
+    cy.url().should('eq', 'https://www.saucedemo.com/');
+
+    // Validate the header text
+    cy.get('.login_logo').should('have.text', 'Swag Labs');
+  };
+
+  it('should log in as standard_user', () => {
+    visitSauceDemo();
+    login('standard_user', 'secret_saucе');
+    validateLoginSuccess();
+  });
+
+  it('should access and open the Sauce Demo website', () => {
+    visitSauceDemo();
+    validateHomePage();
+  });
+
+});
